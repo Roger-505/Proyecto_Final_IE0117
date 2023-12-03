df3349bcbf316462bba247c098913cfc924b6135
BraiderZ <luisjbc2112@gmail.com>
2023-12-03 16:55:19 -0600

    Primera versión del código con paradigma de objetos.

--- logica_preguntas.py
+++ logica_preguntas.py
@@ -1,51 +1,92 @@
 import random
 import time
+import threading
+
+# Clase que encapsula los atributos del juego.
+
+class Pregunta:
+    def __init__(self, enunciado, opciones, respuesta_correcta):
+        self.enunciado = enunciado
+        self.opciones = opciones
+        self.respuesta_correcta = respuesta_correcta
+        
+# Clase que encapsula la logica del juego.
+
+class Juego_preguntas:
+    def __init__(self, preguntas_archivo):
+        self.preguntas = self.cargar_preguntas_y_opciones(preguntas_archivo)
+        self.pregunta_actual = None
+        self.respuesta_correcta = None
 
 # Esta funcion es la encargada de procesar el archivo .txt que almacena las preguntas, opciones, respuestas correctas y devuelve una lista de tuplas con la información.
 
-def cargar_preguntas_y_opciones(nombre_archivo):
-    with open(nombre_archivo, 'r') as archivo:
-        lineas = archivo.readlines()
+    def cargar_preguntas_y_opciones(self, nombre_archivo):
+        with open(nombre_archivo, 'r') as archivo:
+            lineas = archivo.readlines()
 
-    preguntas_y_opciones = []
-    i = 0
-    while i < len(lineas):
-        pregunta = lineas[i].strip()
-        opciones = [lineas[i+j].strip() for j in range(1, 5)]
-        
-        # Esta estructura esta encargada de buscar la línea que contiene la respuesta correcta
-        for j in range(1, 6):
-            if lineas[i+j].startswith("CORRECTA:"):
-                respuesta_correcta = lineas[i+j].split(': ')[1].strip()[0].upper()  # Toma la primera letra y convertirla a mayúscula
-                break
+        preguntas = []
+        i = 0
+        while i < len(lineas):
+            pregunta = lineas[i].strip()
+            opciones = [lineas[i+j].strip() for j in range(1, 5)]
+            
+# Estructura encargada de buscar la línea que contiene la respuesta correcta
 
-        preguntas_y_opciones.append((pregunta, opciones, respuesta_correcta))
-        i += 6  # Avanza al siguiente conjunto de pregunta y opciones
+            for j in range(1, 6):
+                if lineas[i+j].startswith("CORRECTA:"):
+                    respuesta_correcta = lineas[i+j].split(': ')[1].strip()[0].upper()
+                    break
 
-    return preguntas_y_opciones
+            preguntas.append(Pregunta(pregunta, opciones, respuesta_correcta))
+            i += 6 # Avanza al siguiente conjunto de pregunta y opciones
+
+        return preguntas
+
+# Funcion temporizador
+
+    def temporizador(self, tiempo_maximo, pregunta_respondida):
+        time.sleep(tiempo_maximo)
+        if not pregunta_respondida.is_set():
+            print("\n¡Tiempo agotado! Fin del juego.")
+            exit()
+
+    def mostrar_pregunta(self):
+        print(f"Pregunta: {self.pregunta_actual.enunciado}\n")
+        print("Opciones:")
+        for opcion in self.pregunta_actual.opciones:
+            print(f"{opcion}")
+            
+# Esta función esta encargada de proporcionar una interfaz para mostrar las preguntas, opciones y respuesta.
+
+    def jugar(self):
+        print("¡Bienvenido a ¿Quién quiere ser millonario?!")
+        time.sleep(2)  # Delay de 2 segundos
+
+        for idx, pregunta in enumerate(self.preguntas):
+            self.pregunta_actual = pregunta
+            self.respuesta_correcta = pregunta.respuesta_correcta
+
+            print(f"Pregunta {idx + 1}:")
+            self.mostrar_pregunta()
+
+            pregunta_respondida = threading.Event()
+            temporizador_hilo = threading.Thread(target=self.temporizador, args=(10, pregunta_respondida))
+            temporizador_hilo.start()
+
+            try:
+                respuesta = input("Respuesta: ").upper()
+            except ValueError:
+                print("Por favor, ingresa una opcion valida.")
+                exit()
+
+            pregunta_respondida.set()
+
+            if respuesta == self.respuesta_correcta:
+                print("¡Respuesta correcta!")
+            else:
+                print("Respuesta incorrecta. Fin del juego.")
+                exit()
 
 
-# Esta función esta encargada de proporcionar una interfaz para mostrar las preguntas, opciones y respuesta. 
-
-def main():
-    preguntas = cargar_preguntas_y_opciones("preguntas.txt")
-    pregunta_actual = random.choice(preguntas)
-
-    print("¡Bienvenido a ¿Quién quiere ser millonario?!")
-    time.sleep(2)  # Delay de 2 segundos
-    print(f"Pregunta 1: {pregunta_actual[0]}\n")
-    print("Opciones:")
-    for opcion in pregunta_actual[1]:
-        print(f"{opcion}")
-
-    respuesta_correcta = pregunta_actual[2]  # Almacena la respuesta correcta
-
-    respuesta = input("Respuesta: ").upper()
-
-    if respuesta == respuesta_correcta:
-        print("¡Respuesta correcta!")
-    else:
-        print(f"¡Respuesta incorrecta!. La respuesta correcta es {respuesta_correcta}.")
-
-main()
-
+juego = Juego_preguntas("preguntas.txt")
+juego.jugar()
