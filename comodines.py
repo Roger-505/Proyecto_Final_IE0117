0dee3e6859b7e41bb629d73b9758fe5cf37402d5
BraiderZ <luisjbc2112@gmail.com>
2023-12-03 20:05:49 -0600

    Explicación del código.

--- comodines.py
+++ comodines.py
@@ -3,19 +3,21 @@ import random
 class Comodines:
     def __init__(self):
         pass
-
+#Método a modificar en cada clase hija para cumplir con sus diferentes acciones
     def accion_comodin(self, enunciado, preguntas, pregunta_correcta):
         pass
 
 class Mitad(Comodines):
     def accion_comodin(self, enunciado, preguntas, pregunta_correcta):
-        for i in range(len(preguntas)//2):
+        for i in range(len(preguntas)//2): #For con la mitad de las preguntas totales para eliminar la mitad
             while True:
                 eliminar = preguntas[random.randint(0,len(preguntas)-1)]
-                if eliminar != pregunta_correcta:
+                if eliminar != pregunta_correcta: #Verificamos que la respuesta a eliminar no sea la correcta
                     preguntas.remove(eliminar)
                     break
         return preguntas
+
+#Prubea del comodín Mitad
 prueba = Mitad()
 enunciado = "¿Cuál es la función con la que se imprime un mensaje en Python"
 preguntas = ["int", "echo", "print", "for"]
