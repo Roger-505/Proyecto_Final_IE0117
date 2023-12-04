f310bacd1a53779d60518c039cdeda8262e740c8
BraiderZ <luisjbc2112@gmail.com>
2023-12-04 17:51:37 -0600

    Ahora existe una función que invoca los comodines y envía los paámetros correctos.

--- logica_preguntas.py
+++ logica_preguntas.py
@@ -2,8 +2,6 @@ import random
 import time
 import threading
 
-# Clase que encapsula los atributos del juego.
-
 class Pregunta:
     def __init__(self, enunciado, opciones, respuesta_correcta):
         self.enunciado = enunciado
@@ -44,7 +42,8 @@ class Juego_preguntas:
         self.archivos_preguntas = archivos_preguntas
         self.rondas = 7
         self.preguntas = Pregunta.cargar_todas_las_preguntas(archivos_preguntas)
-        self.puntos = 0  # Puntos iniciales
+        self.comodines = ["Mitad","Público","Cambiar pregunta"]
+
 
 # Funcion temporizador
 
@@ -56,12 +55,56 @@ class Juego_preguntas:
 
     def mostrar_pregunta(self):
         print(f"Nivel {self.nivel_actual} - Pregunta: {self.pregunta_actual.enunciado}\n")
-        print("Opciones:")
+        time.sleep(1)
+        print("Opciones (escriba la letra):\n")
         for opcion in self.pregunta_actual.opciones:
             print(f"{opcion}")
-        print("Salir")
+        time.sleep(2)
+        print("\nComodines (Escriba 'comodín'):\n")
+        for i, comodin in enumerate(self.comodines, start=1):
+            print(i,comodin)
+        
+        print("\nSi quiere salir del juego, escriba: Salir\n")
 
-            
+    # Invoca el comodín a utilizar 
+    def invocar_comodines(self):
+        try:
+            eleccion_comodin = str(input("Comodín a elegir: ").upper())
+        except ValueError:
+            print("Por favor, ingresa una opcion valida.")
+                
+        while True:    
+            if eleccion_comodin == "MITAD"  and "Mitad" in self.comodines:
+                mitad = Mitad()
+                mitad.accion_comodin(self.pregunta_actual.opciones, self.pregunta_actual.respuesta_correcta)
+                self.comodines.remove("Mitad")
+                break
+            elif eleccion_comodin == "PÚBLICO"  and "Público" in self.comodines:
+                publico = Publico()
+                publico.accion_comodin(self.pregunta_actual.opciones, self.pregunta_actual.respuesta_correcta)
+                self.comodines.remove("Público")
+                break
+            elif eleccion_comodin == "CAMBIAR PREGUNTA"  and "Cambiar pregunta" in self.comodines:
+                cambio = Cambio_pregunta()
+                #Necesitamos cambiar la respuesta correcta
+                self.pregunta_actual.respuesta_correcta = cambio.accion_comodin(self.nivel_actual, self.pregunta_actual.enunciado)
+                self.comodines.remove("Cambiar pregunta")
+                break
+            else:
+                print("EL comodín que elegista ya fue utilizado o no existe.")
+                try:
+                    eleccion_comodin = str(input("Escriba otro comodín o 'Respuesta' si prefiere no usar comodines: ").upper())
+                except ValueError:
+                    print("Por favor, ingresa una opcion valida.")
+                if eleccion_comodin == "RESPUESTA":
+                    break
+        # Ahora obtenemos la nueva respuesta       
+        try:
+            respuesta = str(input("Nueva respuesta: ").upper())
+        except ValueError:
+            print("Por favor, ingresa una opcion valida.")
+        return respuesta
+
 # Esta función esta encargada de proporcionar una interfaz para mostrar las preguntas, opciones y respuesta.
 
     def jugar(self):
@@ -78,16 +121,18 @@ class Juego_preguntas:
             self.mostrar_pregunta()
 
             pregunta_respondida = threading.Event()
-            temporizador_hilo = threading.Thread(target=self.temporizador, args=(10, pregunta_respondida))
+            temporizador_hilo = threading.Thread(target=self.temporizador, args=(70, pregunta_respondida))
             temporizador_hilo.start()
 
             try:
-                respuesta = input("Respuesta: ").upper()
+                respuesta = str(input("Respuesta: ").upper())
             except ValueError:
                 print("Por favor, ingresa una opcion valida.")
                 exit()
 
             pregunta_respondida.set()
+            if respuesta == "COMODÍN":
+                respuesta = self.invocar_comodines()
 
             if respuesta == "SALIR":
                 print("La seccion de juego ha finalizado.")
@@ -95,13 +140,9 @@ class Juego_preguntas:
 
             if respuesta == self.pregunta_actual.respuesta_correcta:
                 print("¡Respuesta correcta!")
-                self.puntos += 10   # Se suman puntos por cada pregunta correcta
-                print(f"Puntos acumulados: {self.puntos}")
-
             else:
-                print("Respuesta incorrecta. Fin del juego. Puntos acumulados: {self.puntos}")
+                print("Respuesta incorrecta. Fin del juego.")
                 exit()
-
 
 
 archivos_preguntas = ["Nivel_1.txt", "Nivel_2.txt", "Nivel_3.txt",
@@ -109,6 +150,5 @@ archivos_preguntas = ["Nivel_1.txt", "Nivel_2.txt", "Nivel_3.txt",
 
 juego = Juego_preguntas(archivos_preguntas)
 juego.jugar()
-
 
 
